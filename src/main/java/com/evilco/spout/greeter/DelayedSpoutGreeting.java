66ac2e82625f3f64c92fd4c981b8fc39a8695a79
Johannes Donath <johannesd@evil-co.com>
2013-07-25 06:31:02 +0200

    Fixed license header

--- src/main/java/com/evilco/spout/greeter/DelayedSpoutGreeting.java
+++ src/main/java/com/evilco/spout/greeter/DelayedSpoutGreeting.java
@@ -1,22 +1,22 @@
-/*
-* This file is part of SpoutGreeter.
-*
-* Copyright (C) 2013 Evil-Co <http://www.evil-co.com>
-* SpoutGreeter is licensed under the GNU Lesser General Public License.
-*
-* SpoutGreeter is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SpoutPlugin is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ * This file is part of SpoutGreeter.
+ *
+ * Copyright (C) 2013 Evil-Co <http://www.evil-co.com>
+ * SpoutGreeter is licensed under the GNU Lesser General Public License.
+ *
+ * SpoutGreeter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SpoutPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.evilco.spout.greeter;
 
 import java.util.logging.Level;
